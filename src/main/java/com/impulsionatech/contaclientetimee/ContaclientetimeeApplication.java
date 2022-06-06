ccee63e75e1a79da9136bd57a96389c858e869e3
Guilherme <gdsouza.ebt@gmail.com>
2022-06-06 20:23:38 -0300

    upload files config

---
+++ src/main/java/com/impulsionatech/contaclientetimee/ContaclientetimeeApplication.java
@@ -0,0 +1,13 @@
+package com.impulsionatech.contaclientetimee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaclientetimeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaclientetimeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impulsionatech/contaclientetimee/ContaclientetimeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impulsionatech.contaclientetimee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaclientetimeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
